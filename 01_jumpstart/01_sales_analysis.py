f1c30b9832f53e22d3ad8cc26feb3661bfef9852
YangWu1227 <yangwu2020@gmail.com>
2021-09-03 03:29:27 -0400

    "update"

--- 01_jumpstart/01_sales_analysis.py
+++ 01_jumpstart/01_sales_analysis.py
@@ -111,37 +111,149 @@ print(
 
 # 4.0 Joining Data ----
 
+# %% Start Cell
+
+# Check pandas df object type
+print(type(orderlines_df))
+# Convert to pandas data frame object
+orderlines_df = pd.DataFrame(orderlines_df)
+print(type(orderlines_df))
+
+# Drop primary key column
+# The axis argument indicates whether to drop labels from the index or columns
+bikes_orderlines_df = (orderlines_df
+                       .drop(
+                           columns="Unnamed: 0",
+                           axis="column"
+                       )
+                       # Left join bikes data onto orderlines data
+                       .merge(
+                           right=bikes_df, how="left",
+                           left_on="product.id", right_on="bike.id"
+                       )
+                       # Left join bikeship data on two the resultant data
+                       .merge(
+                           right=bikeshops_df, how="left",
+                           left_on="customer.id", right_on="bikeshop.id"))
+
+# %% End Cell
+
 
 # 5.0 Wrangling Data ----
 
+# %% Start Cell
+
 # * No copy
-
-
+df = bikes_orderlines_df
+df
 # * Copy
-
+# Make a copy of this object's indices and data
+# Deafult is deep = TRUE
+# Modifcations to the copy will not be reflected in the original
+df2 = bikes_orderlines_df.copy(deep=True)
+df2
 
 # * Handle Dates
-
+print(type(df["order.date"]))
+# Subset and assignment
+df["order.date"] = pd.to_datetime(df["order.date"])
+# Check type and see if that column is a data time object
+df.info()
 
 # * Show Effect: Copy vs No Copy
+# See that the object "bikes_orderlines_df" is also modified
+# Without copy, Python implements modify-in-place
+# See that the data column is now a datetime object
+# When we use the "=" operator, we create a new binding
+# This binding still references the original object
+# This may be memory efficient but is less safe
+bikes_orderlines_df.info()
 
+# With df.copy(), a new memory allocation is created
+# Python implements copy-on-modify now
+# This doubles the memory usage but preserves the original object
+df2.info()
 
 # * Text Columns
-
+# To see columns more clearly, we may transpose the df
+df.T
 
 # * Splitting Description into category_1, category_2, and frame_material
-
+# Use the split() function to plot text variable into separate ones
+# This gives us a series lists with three elements
+print(df["description"].str.split(pat=" - "))
+# The "expand" argument indicates whether to expand the split strings into separate columns
+print(df["description"].str.split(pat=" - ", expand=True))
+# Now subset multiple elements and assign
+df[["category_1",
+    "category_2",
+    "frame_material"]] = (df["description"]
+                          .str.split(
+        pat=" - ",
+        expand=True
+    ))
+df
 
 # * Splitting Location into City and State
-
+df[["city", "state"]] = (df["location"]
+                         .str.split(
+    pat=", ",
+    expand=True
+))
+df
 
 # * Price Extended
+df["total_revenue"] = df.quantity * df.price
 
+# Order rows by values of revenue (high to low)
+df.sort_values(by="total_revenue", ascending=False)
 
 # * Reorganizing
 
+# Get attributes (colnames) from df object class
+# This returns all the column names
+df.columns
+# This function is used to access the attribute of object
+print(getattr(df, "total_revenue"))
+# Keep only a subset of variable
+# Subset by character index (a list)
+# Use [[ operator for multiple subsetting
+# This creates a copy
+# A new binding is created from the name df to the copied object
+df = df[[
+    'order.id',
+    'order.line',
+    'order.date',
+    # 'customer.id',
+    # 'product.id',
+    'quantity',
+    # 'bike.id',
+    'model',
+    'price',
+    'total_revenue',
+    # 'description',
+    # 'bikeshop.id',
+    'bikeshop.name',
+    # 'location',
+    'category_1',
+    'category_2',
+    'frame_material',
+    'city',
+    'state'
+]]
+
 
 # * Renaming columns
+# Replace all "." with "_"
+df.columns = df.columns.str.replace(
+    pat=".",
+    repl="_",
+)
+
+# Create a new binding from new name to same object
+bikes_orderlines_wrangled = df
+
+# %% End Cell
 
 
 # 6.0 Visualizing a Time Series ----
